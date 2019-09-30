b6ac41027cffb4ef35a22a645a563cb9cd8875b7
thechallenger346 <45742653+thechallenger346@users.noreply.github.com>
2019-09-29 22:05:35 -0400

    Test

---
+++ ticket.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+ <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Alpha Security | HOME</title>
+  <style media="screen">
+  .main{
+   width: 100%;
+   height: auto;
+   position: absolute;
+   left: 0%;
+  }
+   .head{
+    background-color: black;
+    width: 100%;
+    left: 0%;
+    height: 20%;
+    top: 0%;
+    position: absolute;
+   }
+   .Alpha{
+    color: #42a6ff;
+    text-align: center;
+   }
+   .body{
+    background: black;
+    width: 100%;
+    height: 75%;
+    position: absolute;
+    left: 0%;
+    top: 17%;
+   }
+   .footer{
+    height: 20%;
+    width: 100%;
+    position: absolute;
+    left: 0%;
+    background-color: black;
+    bottom: 0%;
+   }
+   .PI{
+    color: blue;
+    font-family: courier;
+    font-size: 100%;
+    font-style: italic;
+    text-align: center;
+   }
+   button{
+    width: 15%;
+    height: 20%;
+   }
+   .ticket{
+    left: 20%;
+    top: 22%;
+    position: absolute;
+   }
+   .home{
+    position: absolute;
+    left: 3%;
+    top: 22%;
+   }
+   .tos{
+    right: 20%;
+    top: 22%;
+    position: absolute;
+   }
+   .contact{
+    position: absolute;
+    right: 3%;
+    top: 22%;
+   }
+  </style>
+ </head>
+ <body>
+<div class="Main">
+ <div class="head">
+  <a href="">
+   <button class="home" name="Home">Home</button>
+  </a>
+  <a href="ticket.php">
+   <buttontab class="ticket" name="Ticket">Open a Ticket</buttontab>
+  </a>
+  <h1 class="Alpha">Alpha Security</h1>
+  <a href="">
+   <button class="tos"name="TOS">Terms of Service</button>
+  </a>
+  <a href="">
+   <button class="contact" name="Contact">Contact Us</button>
+  </a>
+  <h3 class="PI">"Certified Private Investigation Team"</h3>
+ </div>
+ <div class="body">
+
+ </div>
+ <div class="footer">
+
+ </div>
+</div>
+ </body>
+</html>
